40ff14834834868993e0bb0b52c5d5edb581d6fa
fsalzano <francesco.salzano@unimol.it>
2025-04-09 11:12:51 +0200

    add health check

--- hello_api.py
+++ hello_api.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-
 @app.route('/hello', methods=['POST'])
 def hello():
     # Get JSON data from the request
@@ -16,6 +15,9 @@ def hello():
     # Return the greeting
     return jsonify({"message": f"Hello {name}!"})
 
+@app.route('/api/health_check', methods=['GET'])
+def health_check():
+    return jsonify({"status": "ok"}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
